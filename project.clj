818acce3e1043f39d57d87a9f20d8e9bbcf6325d
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2014-03-15 19:04:23 +0200

    Try to force the use of gremlin-java 2.4.0 and blueprints-core 2.4.0

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure                "1.5.1"]
                  [clojurewerkz/support               "0.15.0"]
+                 [com.tinkerpop.gremlin/gremlin-java "2.4.0"]
                  [com.thinkaurelius.titan/titan-all  "0.4.2"]
                  [potemkin "0.3.5-SNAPSHOT"]
-                 [clojurewerkz/ogre "2.3.0.1"]
-                 [clojurewerkz/archimedes "1.0.0-alpha6-SNAPSHOT"]]
+                 [clojurewerkz/ogre "2.3.0.1" :exclusions [com.tinkerpop.blueprints/blueprints-core]]
+                 [clojurewerkz/archimedes "1.0.0-alpha6-SNAPSHOT" :exclusions [com.tinkerpop.blueprints/blueprints-core]]]
   :source-paths  ["src/clojure"]
   :java-source-paths ["src/java"]
   :javac-options     ["-target" "1.6" "-source" "1.6"]
